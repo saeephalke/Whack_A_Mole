3ec685308d4a7ea1ebbb638fa67bda1bfb77bda3
FeliciaChiu <132633991+FeliciaChiu@users.noreply.github.com>
2023-11-09 09:01:24 -0800

    board class getters

--- Board.java
+++ Board.java
@@ -1,32 +1,37 @@
 // not done yet!!
 import java.util.Scanner;  
 public class Board {
-    /*
-private int score;
-private WhackString whackObj;
-private int missCounter;
-Scanner scan = new Scanner(System.in);
+        
+    private int score;
+    private WhackString whackObj;
+    private int missCounter;
+    // Scanner scan = new Scanner(System.in);
 
-public boolean compareIndexValues() {
-  int userIndex = scan.nextInt();
-  int correctIndex = whackObj.index;
-  if(userIndex == correctIndex){
-    return true;
-  }
-  return false;
+public boolean compareIndexValues(int index) {
+    //int userIndex = scan.nextInt();
+    int correctIndex = whackObj.getIndex();
+    if(index == correctIndex){
+        return true;
+      }
+    return false;
 }
 
 public void addPoints() {
-  if (compareIndexValues) {
+  if (compareIndexValues()) {
       score++;
       missCounter = 0;
   } else {
       missCounter++;
   }
 
-  if(missCounter >= 3) {
-    // game over
-  }
 }
-*/
+
+public int getScore() {
+    return score;
+}
+
+
+public int getMissCounter() {
+    return missCounter;
+}
 }
