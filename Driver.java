d7c0433194cc6cc32395665f6d86e10dccd42741
saeephalke <91033007+saeephalke@users.noreply.github.com>
2023-11-09 09:13:38 -0800

    creating driver to testrun the game

--- Driver.java
+++ Driver.java
@@ -1,6 +1,6 @@
 public class Driver {
     public static void main(String[] args) throws Exception {
-        WhackString s = new WhackString();
-        System.out.println(s.createString());
+        PlayGame g = new PlayGame();
+        g.playGame();
     }
 }
