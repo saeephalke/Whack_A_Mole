8cb56bc3301d5f0c330b394cfd2ded076e3f95f0
Sliver3 <149814223+CodingLife1234@users.noreply.github.com>
2023-11-11 16:16:50 -0800

    added the comments.java

--- Driver.java
+++ Driver.java
@@ -1,8 +1,9 @@
 public class Driver {
     /**
-     * runs the play game method to play the game
-     * @param args
-     * @throws Exception
+     * runs the play game method to play and start the game
+     * @param args the command line arguments (not used in this implementation)
+     * @throws Exception if an error occurs during the game execution 
+     * precondition: has all of the information necessary to initialize and run the game
      */
     public static void main(String[] args) throws Exception {
         PlayGame g = new PlayGame();
